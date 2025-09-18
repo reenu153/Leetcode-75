049c80a9dbd05734ef90788915386825cb42447b
Reenu Maria Jerry <54211230+reenu153@users.noreply.github.com>
2025-09-18 13:07:56 +0400

    Time: 3 ms (3.66%) | Memory: 12.5 MB (53.78%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def dp(self,n,memo):
+        if n in memo.keys():
+            return memo[n]
+        print(memo[2])
+
+        memo[n] = self.dp(n-1,memo) + self.dp(n-2,memo)
+        return memo[n]
+
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        memo={1:1,2:2}
+        return self.dp(n,memo)
+
+
+
+    
+
+        
+        
