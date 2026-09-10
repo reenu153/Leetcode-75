a25dd04587055897b0976e37a68136ef21521ad1
Reenu Maria Jerry <54211230+reenu153@users.noreply.github.com>
2026-09-10 18:23:34 +0200

    Time: 19 ms (19.73%) | Memory: 22 MB (64.57%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,20 +1,13 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-
-        anagrams={}
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
+        anagrams={}
+
         for word in strs:
             sorted_word=str(sorted(word))
             if sorted_word in anagrams.keys():
                 anagrams[sorted_word].append(word)
             else:
                 anagrams[sorted_word]=[word]
-        
-        return anagrams.values()
-                
-
-        
+            
+        return list(anagrams.values())
